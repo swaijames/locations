df83ac09b0b55dffc5c32e0c87c8959ae523cab2
swayjames <swaijames255@gmail.com>
2025-04-25 12:42:18 +0300

    v4

--- locations/settings.py
+++ locations/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-+%h-eu-hmx)m#l3xmqjdbo7hibh)5xvf#u_t!b86p5e=^+1(-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
